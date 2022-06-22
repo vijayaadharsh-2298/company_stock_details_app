fb3e02abd72c0e8c4bdcd6a8844302f3263b21f2
Vijay M <vijay.munuswamy@learnship.com>
2022-06-22 22:53:53 +0530

    New commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [companyName, setCompanyName] = useState("INFY");
+  const [companyDetails, setCompanyDetails] = useState([]);
+
+  const COMPANY_NAME_DETAILS = [
+    { id: "INFY", name: "Infosys" },
+    { id: "MSFT", name: "Microsoft" },
+    { id: "IBM", name: "IBM" },
+  ];
+
+  useEffect(() => {
+    if (companyName !== "") {
+      getCompanyDetails(companyName);
+    }
+  }, []);
+
+  const getCompanyDetails = async (companyId) => {
+    setCompanyName(companyId);
+    const response = await axios.get(
+      `http://localhost:3000?companyId=${companyId}`
+    );
+
+    if (response.data && Object.keys(response.data).length > 0) {
+      setCompanyDetails([response.data]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="company-drop-down-list">
+        <label className="company-drop-down-label">
+          Select a company name:
+        </label>{" "}
+        <select
+          value={companyName}
+          onChange={(e) => getCompanyDetails(e.target.value)}
+          className="company-drop-down-select"
         >
-          Learn React
-        </a>
-      </header>
+          {COMPANY_NAME_DETAILS.map((companyDetail) => (
+            <option
+              value={companyDetail.id}
+              className="company-name-option"
+              key={companyDetail.id}
+            >
+              {companyDetail.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="company-details-box">
+        {!!companyDetails && companyDetails.length === 0 ? (
+          <p>Loading...</p>
+        ) : (
+          companyDetails.map((companyDetail) => (
+            <div key={companyDetail.Symbol} className="each-company-details">
+              <h3>{companyDetail.Name}</h3>
+              <p>{companyDetail.Description}</p>
+              <p>{companyDetail.Address}</p>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
